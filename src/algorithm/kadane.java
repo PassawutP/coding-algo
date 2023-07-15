b2f78623b1ac2544eb4c9babd339ac52301ca834
Passawut <passawut.prinyagul@gmail.com>
2023-07-15 19:34:54 +0700

    check

--- src/algorithm/kadane.java
+++ src/algorithm/kadane.java
@@ -16,7 +16,14 @@ public class kadane {
         return max;
     }
     public static void main(String[] args){
+    	// test 1: answer = {4, -1, 2, 1}
     	int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(kadane.maxSubArray(nums));
+        // test 2: answer = {1}
+    	nums = new int[]{1};
+        System.out.println(kadane.maxSubArray(nums));
+        // test 3: answer = {5,4,-1,7,8}
+    	nums = new int[]{5,4,-1,7,8};
         System.out.println(kadane.maxSubArray(nums));
     }
 }
